25d07859b6043981285a594f5a60a47c960b9a52
Scalaptia <fernando@haroware.com>
2023-09-27 14:35:32 -0700

    Update Actividad 6 y 7

--- Clases/Clase 10 (Regresion Lineal y errores).py
+++ Clases/Clase 10 (Regresion Lineal y errores).py
@@ -6,10 +6,9 @@ y=[8, 8, 9, 72, 22, 51, 85, 4, 75, 48, 72, 1, 62, 37, 75, 42, 75, 47, 57, 95]
 
 plt.scatter(x, y)
 
-model = np.polyfit(x, y, 9)
+model = np.polyfit(x, y, 1)
 
 print("Modelo lineal", model)
-
 
 """
 Predicción utilizando poly1d()
@@ -23,7 +22,6 @@ predict(x_value)
 Exactitud del modelo R-squared
 Se debe importar sklearn.metric import r2_score
 """
-from sklearn.metrics import r2_score
 r2 = r2_score(y, predict(x))
 
 print("El valor de r_square es", r2) #coeficiente de determinacion
@@ -36,21 +34,6 @@ x_axis = range(0, 60)
 y_axis = predict(x_axis)
 plt.scatter(x, y)
 plt.plot(x_axis, y_axis, c = "g")
-
-#### Aqui termina el ejercicio para polinomio de grado 9
-
-mymodel = np.poly1d(np.polyfit(x, y, 3))
-
-print("Mi modelo", mymodel)
-predict3 = np.poly1d(mymodel)
-
-x_value = 20
-x_20 = predict3(x_value)
-
-print("Valor de y para x=20 es: ", x_20)
-
-r2_p3 = r2_score(y, predict3(x))
-print("El valor de r_square es", r2) #coeficiente de determinacion
 
 """
 Calculo de los errores y-ypred
@@ -68,10 +51,12 @@ print("Media de los errores = ", media_error)
 ds_error = np.std(errores)
 print("Desviación estándar de los errores = ", ds_error)
 
-""" Histograma de los errores """
+"""
+Histograma de los errores
+"""
 
 plt.title("HISTOGRAMA DE LOS ERRORES")
 plt.xlabel("Articulos en promocion")
 plt.ylabel("Ventas")
-plt.hist(errores)
+plt.hist(errores, bins=7)
 plt.show()
--- Practicas/Practica 6/Ejercicio 1.py
+++ Practicas/Practica 6/Ejercicio 1.py
@@ -1,12 +1,79 @@
+"""
+Practica 6 y 7
+- Encontrar el mejor modelo (aprox. R^2 = |1|)
+- Reportar el código, valor de R^2 obtenido
+- Graficar el histograma de los errores (media y ds del error)
+- Concluir si los errores se aproximan a una distribución normal
+"""
 import numpy as np
-import matplotlib.pylot as plt
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
 
-x= [28, 8, 11, 37, 15, 25, 51, 11, 32, 34, 43, 2, 40, 16, 40, 25, 40, 17, 21, 57]
-y= [8, 8, 9, 75, 22, 51 , 85, 4, 75, 48, 72, 1, 62, 37, 75, 42, 75, 47, 57, 95]
+y = [790, 1160, 929, 865, 1140, 929, 1109, 1365, 1112, 1150, 980, 990, 1112, 1252, 1326, 1330, 1365, 1280, 1119, 1328, 1584, 1428, 1365, 1415, 1415, 1465, 1490, 1725, 1523, 1705, 1605, 1746, 1235, 1390, 1405, 1395]
+x = [99, 95, 95, 90, 105, 105, 90, 92, 98, 99, 99, 101, 99, 94, 97, 97, 99, 104, 104, 105, 94, 99, 99, 99, 99, 102, 104, 114, 109, 114, 115, 117, 104, 108, 109, 120]
 
 plt.scatter(x, y)
 
-model = np.polyfit(x, y, 9)
+model = np.polyfit(x, y, 12)
 
 print("Modelo lineal", model)
 
+"""
+Predicción utilizando poly1d()
+"""
+predict = np.poly1d(model)
+
+x_value = 36
+predict(x_value)
+
+
+"""
+Exactitud del modelo R-squared
+"""
+
+predict = np.poly1d(model)
+r2 = r2_score(y, predict(x))
+
+print("El valor de r_square es", r2)
+
+"""
+Dibujando el modelo
+"""
+
+x_axis = range(90, 120)
+y_axis = predict(x_axis)
+plt.scatter(x, y)
+plt.plot(x_axis, y_axis, c = "g")
+
+mymodel = np.poly1d(np.polyfit(x, y, 3))
+
+print("Mi modelo", mymodel)
+predict3 = np.poly1d(mymodel)
+
+####################################
+
+"""
+Calculo de los errores y-ypred
+"""
+
+errores = []
+for n in y:
+    errores = y - predict(x)
+    
+print("Errores: ", errores)
+
+media_error = np.mean(errores)
+print("Media de los errores = ", media_error)
+
+ds_error = np.std(errores)
+print("Desviación estándar de los errores = ", ds_error)
+
+"""
+Histograma de los errores
+"""
+
+plt.title("HISTOGRAMA DE LOS ERRORES")
+plt.xlabel("CO2")
+plt.ylabel("Weight")
+plt.hist(errores)
+plt.show()
